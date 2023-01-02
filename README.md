0cc176a08041808b79473cba4107ac8dea5e836c
Achmad Ibrahim Humam <55597186+humamisme@users.noreply.github.com>
2023-01-02 08:26:47 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Watchflix
+
+Watchflix is a movie catalog website that helps you find various movies and series around the world.
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <img src="/watchflix.svg" alt="Watchflix logo" className="logo react" />
+      </div>
+      <h1>Watchflix</h1>
+      <p>
+        Watchflix is a movie catalog website that helps you find various movies
+        and series around the world.
+      </p>
+    </div>
+  );
+}
+
+export default App;
