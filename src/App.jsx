761900d8b7ad0588497d25fa5693a99c617166aa
Achmad Ibrahim Humam <55597186+humamisme@users.noreply.github.com>
2023-01-02 10:21:19 +0700

    build: add some dependencies
    Add commitlint, husky, eslint, prettier, tailwind, and set some configuration.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div>
       <div>
-        <img src="/watchflix.svg" alt="Watchflix logo" className="logo react" />
+        <img src="/watchflix.svg" alt="Watchflix logo" className="h-24 w-24" />
       </div>
       <h1>Watchflix</h1>
       <p>
---
+++ tailwind.config.cjs
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          xl: '1440px',
+          '2xl': '1536px',
+        },
+      },
+      colors: {
+        primary: '#00B9AE',
+        dark: {
+          1: '#16181E',
+          2: '#21242D',
+        },
+        light: '#F9F9F9',
+      },
+      fontFamily: {
+        sans: [
+          'Poppins',
+          'Inter',
+          'Avenir',
+          'Helvetica',
+          'Roboto',
+          'Arial',
+          'sans-serif',
+        ],
+      },
+    },
+  },
+  plugins: [],
+};
